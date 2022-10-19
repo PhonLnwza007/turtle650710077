f744717c8656d61b5d500d29b99258d97524e516
Natthaphon Rueangwaraphit <116154713+PhonLnwza007@users.noreply.github.com>
2022-10-19 09:21:00 +0000

    add author

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # turtle650710077
+
+natthaphon rueandwaraphit 650710077
---
+++ main.py
@@ -0,0 +1,8 @@
+import turtle
+
+t = turtle.Turtle()
+
+for c in ['salmon', 'purple', 'blue', 'turquoise', 'pink', 'lightgreen', 'lavender']:
+    t.color(c)
+    t.forward(180)
+    t.left(90)
